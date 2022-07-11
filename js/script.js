ce3eedc02e7a558ff9da035053c94eaf68227fe4
RubyVicente <104641530+rubenfpvicente@users.noreply.github.com>
2022-07-11 13:10:05 +0000

    11/07

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const btnComidas = document.querySelector('.btn-comida');
+const inicio = document.querySelector('.inicio');
+const comidas = document.querySelector('.comidas');
+
+btnComidas.addEventListener('click', function() {
+    inicio.style.display = 'none';
+    comidas.style.display = 'inline';
+});
