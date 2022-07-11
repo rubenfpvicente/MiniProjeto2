e2f636c45e7e30374f633ee9f299550b000a3b0a
RubyVicente <104641530+rubenfpvicente@users.noreply.github.com>
2022-07-11 15:16:30 +0000

    11/07

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 'use strict';
 
 const btnComidas = document.querySelector('.btn-comida');
+const btnBebidas = document.querySelector('.btn-bebida');
+const btnCafetaria = document.querySelector('.btn-cafetaria');
 const inicio = document.querySelector('.inicio');
 const comidas = document.querySelector('.comidas');
+const bebidas = document.querySelector('.bebidas');
+const cafetaria = document.querySelector('.cafetaria');
+const btnInicio = document.querySelector('.btn-back');
+
+const mudarMenu  = function (categoria) {
+    inicio.style.display = 'none';
+    categoria.style.display = 'block';
+}
 
 btnComidas.addEventListener('click', function() {
-    inicio.style.display = 'none';
-    comidas.style.display = 'inline';
+    mudarMenu(comidas);
 });
+
+btnBebidas.addEventListener('click', function() {
+    mudarMenu(bebidas);
+});
+
+btnCafetaria.addEventListener('click', function() {
+    mudarMenu(cafetaria);
+});
+
+btnInicio.addEventListener('click', function() {
+    inicio.style.display = 'block';
+    comidas.style.display = 'none';
+    bebidas.style.display = 'none';
+    cafetaria.style.display = 'none';
+})
